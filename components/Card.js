25584da228be66df55ca3c7cd0b9f59ffad9e847
shawonetc42 <139037447+shawonetc42@users.noreply.github.com>
2024-11-05 01:30:22 +0600

    Update Card.js

--- components/Card.js
+++ components/Card.js
@@ -1,7 +1,13 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 
 const Card = ({ joke }) => {
+  const handleButtonClick = () => {
+    // Add your button click logic here
+    console.log("Button clicked!");
+  };
+
   return (
     <div className="border rounded-lg p-4 shadow-md">
       <Link
@@ -10,8 +16,13 @@ const Card = ({ joke }) => {
       >
         {joke.setup || "Random Joke"}
       </Link>
+      <button
+        className="mt-2 bg-blue-500 text-white rounded p-2"
+        onClick={handleButtonClick}
+      >
+        Handle Button
+      </button>
     </div>
   );
 };
-
 export default Card;
